5e0bd9d7f2645cd2852a19385b0b166593254d06
miltonpepe <miltonfercien@hotmail.com>
2022-04-29 20:24:14 -0500

    Initial commit

---
+++ app/src/main/java/com/example/ejemplo11/Adapter.kt
@@ -0,0 +1,35 @@
+package com.example.ejemplo11
+
+import android.media.Image
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+
+class Adapter (val articulos: List<Resultado>): RecyclerView.Adapter<MainViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return MainViewHolder(layoutInflater.inflate(R.layout.item_articulo, parent,false ))
+    }
+
+    override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+        val articulo : Resultado = articulos.get(position)
+        holder.render(articulo)
+    }
+
+    override fun getItemCount(): Int {
+        return articulos.size
+
+    }
+}
+
+class MainViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+    fun render(articulo: Resultado){
+        view.findViewById<TextView>(R.id.title).text = articulo.title
+        view.findViewById<TextView>(R.id.date).text = articulo.published_date
+        Picasso.get().load(articulo.media[0].mediaMetadata[0].urlMetadata).into(view.findViewById<ImageView>(R.id.ivLogo))
+    }
+}
